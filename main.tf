f61854ff7e6c45b06aaa9a5305a5176699ec0cb2
pavangole <pavangoley5@gmail.com>
2022-02-26 23:59:34 +0530

    Add depends_on keyword because of issues

--- main.tf
+++ main.tf
@@ -8,12 +8,11 @@ resource "tls_private_key" "pk" {
 }
 
 resource "aws_key_pair" "promkey" {
+    depends_on = [tls_private_key.pk]
     key_name = var.ssh_key_name
     public_key = tls_private_key.pk.public_key_openssh
 
-    provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
-    command = "echo '${tls_private_key.pk.private_key_pem}' > ./promkey1.pem"
-  }
+    
 }
 
 resource "aws_security_group" "name" {
@@ -40,6 +39,7 @@ resource "aws_security_group" "name" {
 }
 
 resource "aws_instance" "prom" {
+    depends_on = [aws_key_pair.promkey]
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = var.ssh_key_name
@@ -54,7 +54,34 @@ resource "aws_instance" "prom" {
     }
 
     provisioner "local-exec" {
-    command = "echo ${self.public_ip} >> private_ips.txt"
-  }
+    command = "echo ${self.public_ip} >> ~/workspace/ansible/inventory/ip.py"
+    }
+
+    
+
+    
+  
+}
+
+resource "null_resource" "name" {
+
+    depends_on = [aws_instance.prom]
+    
+    provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./promkey1.pem"
+    }
+
+    provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
+    command = "chmod 400 promkey1.pem"
+    }
+
+    provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
+    command = "sleep 180"
+    }
+
+    provisioner "local-exec" {
+    command = "ansible-playbook -u ec2-user --private-key promkey1.pem ~/workspace/ansible/playbooks/dock_conf.yml"
+    }
+
   
 }
